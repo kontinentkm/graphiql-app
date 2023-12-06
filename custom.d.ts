0fcb7fdee3e8e65d67986438b3334f767be23893
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-12-06 21:52:41 +0300

    feat: add aliases, custom modules types and redux

---
+++ custom.d.ts
@@ -0,0 +1,34 @@
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.html' {
+  const content: string;
+  export default content;
+}
