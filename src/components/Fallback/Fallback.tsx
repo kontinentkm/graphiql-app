70a49d29ffc17ba7b12fdcfc64420765bb6cc95e
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-12-10 20:33:14 +0300

    feat: add basic styles to 404 and error pages

--- src/components/Fallback/Fallback.tsx
+++ src/components/Fallback/Fallback.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { useLocation, useNavigate, useRouteError } from 'react-router-dom';
 
+import styles from '@src/components/Fallback/Fallback.module.css';
+
 const FALLBACK_MESSAGE = 'Error occurs!';
 const UNKNOWN_ERROR_MESSAGE = 'Unknown error';
 const BTN_TEXT = 'Reload';
@@ -21,9 +23,9 @@ const Fallback: FC = (): JSX.Element => {
   const onBtnClick = (): void => navigate(location.pathname);
 
   return (
-    <div>
-      <p>{FALLBACK_MESSAGE}</p>
-      <p>{message}</p>
+    <div className={styles.fallback}>
+      <p className={styles.title}>{FALLBACK_MESSAGE}</p>
+      <p className={styles.message}>{`Description: ${message}`}</p>
       <button type="button" onClick={onBtnClick}>
         {BTN_TEXT}
       </button>
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from '@src/pages/NotFound/NotFound.module.css';
+
 import EPages from '@src/types/enums/EPages';
 
 const FALLBACK_MESSAGE = '404 Page Not Found!';
@@ -8,9 +10,11 @@ const LINK_TEXT = 'Go to welcome page >';
 
 const NotFound: FC = (): JSX.Element => {
   return (
-    <div>
-      <p>{FALLBACK_MESSAGE}</p>
-      <Link to={EPages.WELCOME}>{LINK_TEXT}</Link>
+    <div className={styles.notFound}>
+      <p className={styles.title}>{FALLBACK_MESSAGE}</p>
+      <Link className={styles.link} to={EPages.WELCOME}>
+        {LINK_TEXT}
+      </Link>
     </div>
   );
 };
