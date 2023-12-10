733752ef52bf69067cf1a68ad060f908e9c15657
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-12-10 16:46:30 +0300

    feat: add footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,12 +3,15 @@ import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import styles from '@src/components/Layout/Layout.module.css';
+
 import EPages from '@src/types/enums/EPages';
 
 import { capitalize } from '@src/utils/StringTransform';
 
 import { APP_TITLE } from '@src/constants/global';
 import favicon from '@src/assets/graphql.png';
+
+import Footer from '@src/components/Footer/Footer';
 
 const Layout: FC = (): JSX.Element => {
   const { pathname } = useLocation();
@@ -35,6 +38,8 @@ const Layout: FC = (): JSX.Element => {
           <Outlet />
         </div>
       </main>
+
+      <Footer />
     </HelmetProvider>
   );
 };
