c78903d2815f0250b3c654f82151bc5ee35bd944
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-05 22:05:19 +0300

    feat: add appropiate test coverage setup

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   clearMocks: true,
-  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
   coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    'vite-env.d.ts',
+    'constants',
+    'types',
+    'main.tsx',
+    'App.tsx',
+  ],
   testEnvironment: 'jest-environment-jsdom',
   preset: 'ts-jest',
   modulePaths: ['<rootDir>'],
@@ -11,7 +18,7 @@ const config: Config = {
     '.+\\.(css|scss|png|jpg|svg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^@src(.*)$': '<rootDir>/src$1',
   },
-  modulePathIgnorePatterns: ['__mocks__', 'types'],
+  modulePathIgnorePatterns: ['__mocks__'],
   setupFilesAfterEnv: ['./jest-setup.ts'],
 };
 
